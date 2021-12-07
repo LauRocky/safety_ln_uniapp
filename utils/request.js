64b5a5790b4ee243b027f416fe18d51aaa98e4ae
zengzixu <18616364770@163.com>
2021-12-07 11:16:29 +0800

    request

---
+++ utils/request.js
@@ -0,0 +1,72 @@
+// 
+const baseUrl = ''
+// 
+export function request( url, type, date, tips ) {
+	// console.log(url, type, date)
+	// 默认为开启错误提示
+	if (tips == undefined) {
+		tips = {
+			showError: true, //开启错误提示
+		}
+	}
+	return new Promise((resolve, reject) => {
+		uni.request({
+			url: baseUrl+ 'api/' + url, //由基础路径和接口地址
+			method: type  || "GET", //请求的方式必须大写
+			data: date  || {}, //参数
+			header: {
+				'content-type': 'application/x-www-form-urlencoded',
+				'Authori-zation': uni.getStorageSync('token')
+			},
+			// 成功使用resolve
+			success: (res) => {
+				if (res.data && res.data.code ==1) {
+					resolve(res)
+				} else {
+					/**
+					 * 请求失败，是否要提示出来;
+					 * showError: true,开启错误提示
+					 *  showError: false,关闭错误提示
+					 * */
+					if (tips.showError) {
+						showError(res);
+					}
+					resolve(res)
+				}
+			},
+			//失败调用reject，
+			fail: (err) => {
+				// 失败做处理
+				if (tips.showError) {
+					showError(res);
+				}
+				reject(err)
+			}
+		});
+	})
+}
+
+// 错误处理
+const showError = (error) => {
+	// console.log(error)
+	if (error.data.msg == 'token失效_01'){
+		uni.removeStorage('token');
+		uni.showToast({
+			title: '请重新登录',
+			icon: 'none',
+			duration: 2000
+		});
+		setTimeout(()=>{
+			uni.reLaunch({
+				url: ''
+			})
+		},2000)
+	} else {
+		uni.showToast({
+			title: error.data.msg,
+			icon: 'none',
+			duration: 1000
+		});
+		
+	}
+}
