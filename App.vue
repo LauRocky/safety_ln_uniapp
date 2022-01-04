acad7f367a4ccf33e0feedb54449bc7db1742b2d
zengzixu <18616364770@163.com>
2022-01-04 13:36:37 +0800

    ‘修改ios唤起2 git commit -m ‘修改ios唤起2

--- App.vue
+++ App.vue
@@ -1,75 +1,71 @@
 <script>
-	export default {
-		globalData:{
-			version:""
-		},
-		onLaunch:function(){
-			// #ifdef APP-PLUS
-			plus.runtime.getProperty(plus.runtime.appid,(wgtinfo)=>{
-				this.globalData.version = wgtinfo.version
-			})
-			// #endif
-			
-		},
-		onShow:function(){
+export default {
+	globalData: {
+		version: ''
+	},
+	onLaunch: function() {
+		// #ifdef APP-PLUS
+		plus.runtime.getProperty(plus.runtime.appid, wgtinfo => {
+			this.globalData.version = wgtinfo.version;
+		});
+		// #endif
+	},
+	onShow: function() {
 		
+	},
+	mounted: function() {},
+	methods: {
+		toLogin() {
+			uni.navigateTo({
+				url: '/pages/login/index'
+			});
 		},
-		mounted:function(){
-			
-		},
-		methods:{
-			toLogin() {
-				uni.navigateTo({
-					url: "/pages/login/index"
-				})
-			},
-			getCode(code) {
-				uni.showLoading({
-					title:"正在登陆中"
-				});
-				this.$http(
-					'/lvxin/loginByCode',
-					'POST', {
-						"code": code
-					}
-				).then(res => {
+		getCode(code) {
+			uni.showLoading({
+				title: '正在登陆中'
+			});
+			this.$http('/lvxin/loginByCode', 'POST', {
+				code: code
+			})
+				.then(res => {
 					uni.hideLoading();
 					console.log(res);
 					if (res.code == 0) {
 						uni.setStorageSync('userInfo', res.user);
 						uni.setStorageSync('token', res.token);
-			
+
 						uni.switchTab({
 							url: '/pages/home/index'
 						});
-					}else{
+					} else {
 						uni.showToast({
-							icon:"error",
-							duration:1500,
-							title:"登录失败"
+							icon: 'error',
+							duration: 1500,
+							title: '登录失败'
 						});
 					}
-				}).catch(err => {
+				})
+				.catch(err => {
 					uni.hideLoading();
-					console.log(err)
+					console.log(err);
 					uni.hideLoading();
 					uni.showToast({
-						icon:"error",
-						duration:1500,
-						title:"登录失败"
+						icon: 'error',
+						duration: 1500,
+						title: '登录失败'
 					});
-				})
-			}
+				});
 		}
 	}
+};
 </script>
 
 <style lang="scss">
-	@import "uview-ui/index.scss";
-	/*每个页面公共css */
-	html body{
-		width: 100%;
-		height: 100%;
-		background-color: #F6F8F7;
-	}
+@import 'uview-ui/index.scss';
+/*每个页面公共css */
+html body {
+	width: 100%;
+	height: 100%;
+	background-color: #f6f8f7;
+}
 </style>
