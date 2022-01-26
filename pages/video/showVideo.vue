3cbb7d852dbe81c5e91f9e4e7bda10d575387186
cuihao <ch13611301359@outlook.com>
2022-01-26 13:52:48 +0800

    准备同步

--- pages/video/showVideo.vue
+++ pages/video/showVideo.vue
@@ -5,18 +5,15 @@
 		<view class="list">
 			<!-- 搜索框 -->
 			<view style="padding:30upx 30upx 0">
-				<u-search class="searchs" v-model="seach" @search="handsearch" shape="round" height="50"
-					bgColor="#ffffff" searchIconColor="#333333" :showAction="false"></u-search>
+				<u-search class="searchs" v-model="seach" @search="handsearch" shape="round" height="50" bgColor="#ffffff" searchIconColor="#333333" :showAction="false"></u-search>
 			</view>
-			<view v-if="this.showList.length == 0">
-				<image class="kong" src="../../static/danger/kong.png" mode=""></image>
-			</view>
+			<view v-if="this.showList.length == 0"><image class="kong" src="../../static/danger/kong.png" mode=""></image></view>
 			<view class="videolist" v-else>
 				<view class="item" v-for="(item, index) in showList" :key="index" @click="videodetail(item)">
 					<text class="item-status">
 						<view class="item-color" v-if="item.status == 0" style="background-color:#E43D33;"></view>
 						<view class="item-color" v-else></view>
-						{{ item.status == 0 ? '不在线' : '在线' }}
+						<text class="item-text">{{item.status==0?'不在线':'在线'}}</text>
 					</text>
 					<image class="imgs" :src="item.image" mode="" @error="img" :data-index="index"></image>
 					<view class="mask"></view>
@@ -27,307 +24,307 @@
 	</view>
 </template>
 <script>
-	import navBar from '../../components/navBar/navBar.vue';
-	import twoNavbar from '../../components/TwoNavbar/TwoNavbar.vue';
-	import {
-		request
-	} from '../../utils/request.js';
-	import {
-		scanCode,
-		is_iOS
+import navBar from '../../components/navBar/navBar.vue';
+import twoNavbar from '../../components/TwoNavbar/TwoNavbar.vue';
+import { request } from '../../utils/request.js';
+import {
+		scanCode,is_iOS
 	} from '../../utils/utils.js';
-	export default {
-		components: {
-			navBar,
-			twoNavbar
-		},
-		data() {
-			return {
-				showImg: require('../../static/video/detailVideo.png'),
-				seach: '',
-				project: {
-					projectId: '',
-					projectName: ''
-				},
-				ehomeList: [],
-				flag: false,
-				etEznew: '',
-				showList: [],
-				rawList: []
-			};
-		},
-		methods: {
-			img(e) {
-				console.log(e);
-				const index = e.target.dataset.index;
-				this.showList[index].image = this.showImg;
+export default {
+	components: {
+		navBar,
+		twoNavbar
+	},
+	data() {
+		return {
+			showImg: require('../../static/video/detailVideo.png'),
+			seach: '',
+			project: {
+				projectId: '',
+				projectName: ''
 			},
-			handsearch(val) {
-				if (this.rawList) {
-					this.showList = this.rawList;
-					if (val) {
-						let result = [];
-						this.showList.forEach(e => {
-							let pName = e.ipcName;
-							if (pName.indexOf(val) > -1) {
-								result.push(e);
-							}
-						});
-						this.showList = result;
-					}
+			ehomeList: [],
+			flag: false,
+			etEznew: '',
+			showList: [],
+			rawList: []
+		};
+	},
+	methods: {
+		img(e) {
+			console.log(e);
+			const index = e.target.dataset.index;
+			this.showList[index].image = this.showImg;
+		},
+		handsearch(val) {
+			if (this.rawList) {
+				this.showList = this.rawList;
+				if (val) {
+					let result = [];
+					this.showList.forEach(e => {
+						let pName = e.ipcName;
+						if (pName.indexOf(val) > -1) {
+							result.push(e);
+						}
+					});
+					this.showList = result;
 				}
-			},
-			videodetail(item) {
-
-				// 如果设备处于离线,弹出提示,不让他跳转
-				if (item.status == 0) {
+			}
+		},
+		videodetail(item) {
+		
+			// 如果设备处于离线,弹出提示,不让他跳转
+			if (item.status == 0) {
+				uni.showToast({
+					title: '当前设备离线',
+					icon: 'none'
+				});
+			} else {
+				
+				if (item.cameraIndexCode) {
+					//安卓端不需要跳转。需判断。
+					if(is_iOS()){
+					uni.navigateTo({
+						url: `/pages/video/detailVideo?ezv=${0}&camera=${item.cameraIndexCode}&names=${item.ipcName}&ipcId=${item.ipcId}`
+					});
+					}else{
+						//以下代码在安卓端生效
+						let url = "/ehome/camera/previewurl/rtsp/rtsp/" + item.cameraIndexCode;
+						console.error(url);
+						request(url,
+								'POST', {}, false)
+							.then(res => {
+								let apiUrl = 'https://esq.cgdg.com';
+											
+								let body = {
+									'stream': 'rtsp',
+									'type': 'video',
+									'body': res,
+									'cameraIndexCode': item.cameraIndexCode,
+									'channel': null,
+									'nvr': null,
+									'token': uni.getStorageSync('token'),
+									"ezvizAccountId":null,
+									"name":item.ipcName,
+									"id":item.ipcId
+								};
+								uni.sendNativeEvent(JSON.stringify(body), rest => {
+									console.log(rest);
+								});
+											
+							}).catch(e => {
+								console.error(e);
+								uni.showToast({
+									title: '获取播放地址失败',
+									icon: 'none'
+								})
+							})
+					}
+					
+					
+					
+				} else if (item.ezvizAccountId) {
+					//安卓端不需要跳转。需判断。
+					if(is_iOS()){
+						uni.navigateTo({
+							url: `/pages/video/detailVideo?ezv=${1}&nvr=${item.nvrDeviceSerial}&ezviz=${item.ezvizAccountId}&names=${item.ipcName}&channel=${item.channel}`
+						});
+					}else{
+					//以下代码在安卓端生效
+					let url =
+						`/getEzNewLiveAddress/${item.nvrDeviceSerial}/${item.channel}/${item.ezvizAccountId}/2`;
+					console.error(url)
+					request(url,
+							'POST', {}, false)
+						.then(res => {
+							console.error(res);
+							if (res.result.code == 20007) {
+								uni.showToast({
+									title: '设备离线',
+									icon: 'none'
+								})
+							} else {
+										
+								let body = {
+									'stream': 'hls',
+									'body': res,
+									'type': 'video',
+									'cameraIndexCode': null,
+									'channel': item.channel,
+									'nvr': item.nvrDeviceSerial,
+									'token': uni.getStorageSync('token'),
+									"ezvizAccountId":item.ezvizAccountId,
+									"name":item.ipcName,
+									"id":item.ipcId
+								};
+								uni.sendNativeEvent(JSON.stringify(body), rest => {
+									console.log(rest);
+								});
+										
+							}
+						})
+						.catch(e => {
+							console.error(e);
+							uni.showToast({
+								title: '获取播放地址失败',
+								icon: 'none'
+							})
+						})	
+					}
+					
+					
+				} else {
 					uni.showToast({
-						title: '当前设备离线',
+						title: '获取播放地址失败',
 						icon: 'none'
 					});
-				} else {
-
-					if (item.cameraIndexCode) {
-						//安卓端不需要跳转。需判断。
-						if (is_iOS()) {
-							uni.navigateTo({
-								url: `/pages/video/detailVideo?ezv=${0}&camera=${item.cameraIndexCode}&names=${item.ipcName}&ipcId=${item.ipcId}`
-							});
-						} else {
-							//以下代码在安卓端生效
-							let url = "/ehome/camera/previewurl/rtsp/rtsp/" + item.cameraIndexCode;
-							console.error(url);
-							request(url,
-									'POST', {}, false)
-								.then(res => {
-									let apiUrl = 'https://esq.cgdg.com';
-
-									let body = {
-										'stream': 'rtsp',
-										'type': 'video',
-										'body': res,
-										'cameraIndexCode': item.cameraIndexCode,
-										'channel': null,
-										'nvr': null,
-										'token': uni.getStorageSync('token'),
-										"ezvizAccountId": null,
-										"name": item.ipcName,
-										"id": item.ipcId
-									};
-									uni.sendNativeEvent(JSON.stringify(body), rest => {
-										console.log(rest);
-									});
-
-								}).catch(e => {
-									console.error(e);
-									uni.showToast({
-										title: '获取播放地址失败',
-										icon: 'none'
-									})
-								})
-						}
-
-
-
-					} else if (item.ezvizAccountId) {
-						//安卓端不需要跳转。需判断。
-						if (is_iOS()) {
-							uni.navigateTo({
-								url: `/pages/video/detailVideo?ezv=${1}&nvr=${item.nvrDeviceSerial}&ezviz=${item.ezvizAccountId}&names=${item.ipcName}&channel=${item.channel}`
-							});
-						} else {
-							//以下代码在安卓端生效
-							let url =
-								`/getEzNewLiveAddress/${item.nvrDeviceSerial}/${item.channel}/${item.ezvizAccountId}/2`;
-							console.error(url)
-							request(url,
-									'POST', {}, false)
-								.then(res => {
-									console.error(res);
-									if (res.result.code == 20007) {
-										uni.showToast({
-											title: '设备离线',
-											icon: 'none'
-										})
-									} else {
-
-										let body = {
-											'stream': 'hls',
-											'body': res,
-											'type': 'video',
-											'cameraIndexCode': null,
-											'channel': item.channel,
-											'nvr': item.nvrDeviceSerial,
-											'token': uni.getStorageSync('token'),
-											"ezvizAccountId": item.ezvizAccountId,
-											"name": item.ipcName,
-											"id": item.ipcId
-										};
-										uni.sendNativeEvent(JSON.stringify(body), rest => {
-											console.log(rest);
-										});
-
-									}
-								})
-								.catch(e => {
-									console.error(e);
-									uni.showToast({
-										title: '获取播放地址失败',
-										icon: 'none'
-									})
-								})
-						}
-
-
-					} else {
-						uni.showToast({
-							title: '获取播放地址失败',
-							icon: 'none'
-						});
-					}
 				}
-			},
-			back() {
-				uni.navigateBack({
-					delta: 2
-				});
-			},
-			// 2. 获取特定项目下的摄像头列表
-			show() {
-				this.$http(
-						'/camera/project/show',
-						'POST', {
-							projectId: this.project.projectId
-						},
-						false
-					)
-					.then(res => {
-						console.log(res);
-						if (res.code == 0) {
-							this.rawList = res.projectInfoEntities[0].cameraEntities;
-							this.showList = res.projectInfoEntities[0].cameraEntities;
-							console.log('da', this.showList);
-						}
-					})
-					.catch(err => {
-						console.log(err);
-					});
 			}
-			// 传值方式  this.$http(`/ehome/camera/previewurl/hls/${el.cameraIndexCode}`,'POST',{},false)
 		},
-		onLoad(options) {
-			this.project.projectId = options.projectId;
-			this.project.projectName = options.projectName;
-			// uni.setNavigationBarTitle({
-			// 	title:this.project.projectName
-			// })
+		back() {
+			uni.navigateBack({
+				delta: 2
+			});
 		},
-		onShow() {
-			this.show();
+		// 2. 获取特定项目下的摄像头列表
+		show() {
+			this.$http(
+				'/camera/project/show',
+				'POST',
+				{
+					projectId: this.project.projectId
+				},
+				false
+			)
+				.then(res => {
+					console.log(res);
+					if (res.code == 0) {
+						this.rawList = res.projectInfoEntities[0].cameraEntities;
+						this.showList = res.projectInfoEntities[0].cameraEntities;
+						console.log('da', this.showList);
+					}
+				})
+				.catch(err => {
+					console.log(err);
+				});
 		}
-	};
+		// 传值方式  this.$http(`/ehome/camera/previewurl/hls/${el.cameraIndexCode}`,'POST',{},false)
+	},
+	onLoad(options) {
+		this.project.projectId = options.projectId;
+		this.project.projectName = options.projectName;
+		// uni.setNavigationBarTitle({
+		// 	title:this.project.projectName
+		// })
+	},
+	onShow() {
+		this.show();
+	}
+};
 </script>
 
 <style lang="scss" scoped>
-	.video {
-		overflow: hidden;
+.video {
+	overflow: hidden;
 
-		.list {
-			.searchs {
+	.list {
+		.searchs {
+			width: 92vw;
+			margin: 0 auto;
+			height: 60upx;
+			background: #ffffff;
+			border: 1upx solid #333333;
+			opacity: 0.4;
+			border-radius: 30upx;
+		}
+
+		.ipt {
+			margin: 30upx auto;
+			width: 92vw;
+			height: 60upx;
+			background: #ffffff;
+			padding-left: 24upx;
+			font-size: 28upx;
+			font-family: PingFang SC;
+			font-weight: 400;
+			color: #333333;
+			opacity: 0.8;
+			border: 1upx solid #333333;
+			opacity: 0.4;
+			border-radius: 30upx;
+		}
+
+		.videolist {
+			.item {
+				position: relative;
 				width: 92vw;
-				margin: 0 auto;
-				height: 60upx;
-				background: #ffffff;
-				border: 1upx solid #333333;
-				opacity: 0.4;
-				border-radius: 30upx;
-			}
+				margin: 20upx auto;
+				height: 379upx;
+				.imgs {
+					width: 100%;
+					height: 100%;
+					border-radius: 10upx;
+				}
 
-			.ipt {
-				margin: 30upx auto;
-				width: 92vw;
-				height: 60upx;
-				background: #ffffff;
-				padding-left: 24upx;
-				font-size: 28upx;
-				font-family: PingFang SC;
-				font-weight: 400;
-				color: #333333;
-				opacity: 0.8;
-				border: 1upx solid #333333;
-				opacity: 0.4;
-				border-radius: 30upx;
-			}
-
-			.videolist {
-				.item {
-					position: relative;
+				.mask {
+					position: absolute;
+					// left: 19upx;
+					bottom: 0;
 					width: 92vw;
-					margin: 20upx auto;
-					height: 379upx;
+					height: 60upx;
+					background: #000000;
+					opacity: 0.6;
+					border-radius: 0px 0px 10upx 10upx;
+				}
 
-					.imgs {
-						width: 100%;
-						height: 100%;
-						border-radius: 10upx;
-					}
-
-					.mask {
+				.mask-name {
+					position: absolute;
+					left: 30upx;
+					bottom: 12upx;
+					font-size: 28upx;
+					font-family: PingFang SC;
+					font-weight: bold;
+					color: #ffffff;
+				}
+				.item-status{
+					position: absolute;
+					right: 0upx;
+					top: 0upx;
+					z-index: 5;
+					width: 110upx; 
+					height: 50upx;
+					font-family: PingFang SC;
+					font-size: 24upx;
+					font-weight: bold;
+					color: #FFFFFF;
+					line-height: 50upx;
+				
+					border-radius: 10upx;
+					background-color: rgba(0,0,0,0.3);
+					.item-color{
 						position: absolute;
-						// left: 19upx;
-						bottom: 0;
-						width: 92vw;
-						height: 60upx;
-						background: #000000;
-						opacity: 0.6;
-						border-radius: 0px 0px 10upx 10upx;
+						top: 18upx;
+						left: 16upx;
+						width: 15upx;
+						height: 15upx;
+						border-radius: 50%;
+						background-color: #00B48F;
 					}
-
-					.mask-name {
+					.item-text{
 						position: absolute;
-						left: 30upx;
-						bottom: 12upx;
-						font-size: 28upx;
-						font-family: PingFang SC;
-						font-weight: bold;
-						color: #ffffff;
-					}
-
-					.item-status {
-						position: absolute;
-						right: 0upx;
-						top: 0upx;
-						z-index: 5;
-						width: 110upx;
-						height: 50upx;
-						font-family: PingFang SC;
-						font-size: 24upx;
-						font-weight: bold;
-						color: #ffffff;
-						line-height: 50upx;
-						text-align: center;
-						border-radius: 10upx;
-						background-color: rgba(0, 0, 0, 0.3);
-
-						.item-color {
-							position: absolute;
-							top: 20upx;
-							left: 4upx;
-							width: 10upx;
-							height: 10upx;
-							border-radius: 50%;
-							background-color: #00b48f;
-						}
+						
+						left: 40upx;
+					
 					}
 				}
 			}
-
-			.kong {
-				position: fixed;
-				top: 50%;
-				left: 50%;
-				transform: translate(-50%, -50%);
-			}
+		}
+		.kong {
+			position: fixed;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
 		}
 	}
+}
 </style>
