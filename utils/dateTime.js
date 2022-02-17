a909cb46dfc53351c79224354820ea262d23696f
zengzixu <18616364770@163.com>
2022-02-17 15:08:13 +0800

    修改录像回放

---
+++ utils/dateTime.js
@@ -0,0 +1,14 @@
+export function getTime(val) {
+	//获取当前时间
+	let	date = val?new Date(val):new Date(),
+	year = date.getFullYear(),
+		month = date.getMonth() + 1,
+		day = date.getDate(),
+		hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours(),
+		minute = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes(),
+		second = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+	month >= 1 && month <= 9 ? (month = "0" + month) : "";
+	day >= 0 && day <= 9 ? (day = "0" + day) : "";
+	var timer = year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
+	return timer;
+}
